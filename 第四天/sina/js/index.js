82f9c35229dddf29021547b9ebf21d4d2c77261d
renjinhui <1227760869@qq.com>
2018-09-02 17:10:58 +0800

    m

--- 第四天/sina/js/index.js
+++ 第四天/sina/js/index.js
@@ -103,7 +103,7 @@ p.then(function (data) {
 
 var listPro = new Promise(function (resolve, reject) {
    $.ajax({
-       type:'post',
+       type:'get',
        url:'./data/list.json',
        data:{t:1},
        success:function (data) {
